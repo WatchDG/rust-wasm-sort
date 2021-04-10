00fe6bcab2a190dd90243b2be681033cfb03777b
WatchDG <baranov@w3m.co>
2021-04-10 21:13:12 +0300

    add bubble_sort

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,7 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn bubble_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
-    let mut v = a.to_vec();
+pub fn bubble_sort<T: PartialOrd>(v: &mut Vec<T>) {
     let l = v.len();
     for i in 0..(l - 1) {
         for j in (i + 1)..l {
@@ -18,5 +16,11 @@ pub fn bubble_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
             }
         }
     }
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
     v
 }
