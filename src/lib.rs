6bf1c19e7c9e03574fe17d985f8829bc44157800
WatchDG <baranov@w3m.co>
2021-04-11 11:18:57 +0300

    0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,55 @@ pub fn bubble_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
     v
 }
 
+#[wasm_bindgen]
+pub fn bubble_sort_int16array(a: &js_sys::Int16Array) -> Vec<i16> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_int8array(a: &js_sys::Int8Array) -> Vec<i8> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_uint32array(a: &js_sys::Uint32Array) -> Vec<u32> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_uint16array(a: &js_sys::Uint16Array) -> Vec<u16> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_uint8array(a: &js_sys::Uint8Array) -> Vec<u8> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_float64array(a: &js_sys::Float64Array) -> Vec<f64> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn bubble_sort_float32array(a: &js_sys::Float32Array) -> Vec<f32> {
+    let mut v = a.to_vec();
+    bubble_sort(&mut v);
+    v
+}
+
 pub fn merge_sort<T: PartialOrd + Clone>(v: &mut [T]) -> Vec<T> {
     let l = v.len();
     if l < 2 {
@@ -61,6 +110,48 @@ pub fn merge_sort<T: PartialOrd + Clone>(v: &mut [T]) -> Vec<T> {
 
 #[wasm_bindgen]
 pub fn merge_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_int16array(a: &js_sys::Int16Array) -> Vec<i16> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_int8array(a: &js_sys::Int8Array) -> Vec<i8> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_uint32array(a: &js_sys::Uint32Array) -> Vec<u32> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_uint16array(a: &js_sys::Uint16Array) -> Vec<u16> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_uint8array(a: &js_sys::Uint8Array) -> Vec<u8> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_float64array(a: &js_sys::Float64Array) -> Vec<f64> {
+    let mut v = a.to_vec();
+    merge_sort(&mut v)
+}
+
+#[wasm_bindgen]
+pub fn merge_sort_float32array(a: &js_sys::Float32Array) -> Vec<f32> {
     let mut v = a.to_vec();
     merge_sort(&mut v)
 }
