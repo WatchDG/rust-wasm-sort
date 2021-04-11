6a8856737843c035a99e7ed620911d356c0db8d7
WatchDG <baranov@w3m.co>
2021-04-11 21:35:58 +0300

    add improved_bubble_sort

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,85 @@ pub fn bubble_sort_float32array(a: &js_sys::Float32Array) -> Vec<f32> {
     v
 }
 
+pub fn improved_bubble_sort<T: PartialOrd>(v: &mut Vec<T>) {
+    let l = v.len();
+    for j in 0..(l - 1) {
+        let mut f = true;
+        let mut min = j;
+        for i in j..(l - j - 1) {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                f = false;
+            }
+            if v[i] < v[min] {
+                min = i;
+            }
+        }
+        if f {
+            break;
+        }
+        if j != min {
+            v.swap(j, min);
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_int32array(a: &js_sys::Int32Array) -> Vec<i32> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_int16array(a: &js_sys::Int16Array) -> Vec<i16> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_int8array(a: &js_sys::Int8Array) -> Vec<i8> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_uint32array(a: &js_sys::Uint32Array) -> Vec<u32> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_uint16array(a: &js_sys::Uint16Array) -> Vec<u16> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_uint8array(a: &js_sys::Uint8Array) -> Vec<u8> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_float64array(a: &js_sys::Float64Array) -> Vec<f64> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
+#[wasm_bindgen]
+pub fn improved_bubble_sort_float32array(a: &js_sys::Float32Array) -> Vec<f32> {
+    let mut v = a.to_vec();
+    improved_bubble_sort(&mut v);
+    v
+}
+
 pub fn selection_sort<T: PartialOrd>(v: &mut Vec<T>) {
     let l = v.len();
     for i in 0..(l - 1) {
